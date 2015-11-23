6682df8681ce5c26a4479d0bc1b49941e7a34bcc
Sayyadtajuddin <tajrazak>
2015-11-23 20:24:45 +0530

    dinner dash added code

--- www/controllers/navBackController.js
+++ www/controllers/navBackController.js
@@ -1,14 +1,17 @@
-app.controller('navBackCtrl',function($scope, $ionicHistory,$location,$rootScope,$stateParams){
+app.controller('navBackCtrl',function($scope, $ionicHistory,$location,$rootScope,$stateParams,$ionicPlatform){
 
   $rootScope.title='Back';
   $rootScope.maintitle="DinnerDash"
   $scope.goBack=function(){
+//console.log($ionicPlatform);
+
     console.log($location.path()+","+$stateParams.type+','+$stateParams.title)
     if($location.path()!='/home')
     {
       $rootScope.visible=true;
       $ionicHistory.goBack();
       $rootScope.maintitle="DinnerDash";
+    //console.log($rootScope.maintitle)
     }
   }
 
